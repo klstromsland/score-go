3cb73b4b5766a08a5451adb11c101e79077f8ca9
trickling <karilynnie@comcast.net>
2016-05-01 10:36:45 -0700

    Procfile

--- static/jscript/getTimeORG.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {		
-//  var timeout_m = {{ .Maxtime_m }};
-//  var timeout_s = {{ .Maxtime_s }};
-//  var timeout_ms = {{ .Maxtime_ms }};
-//  var timeout = (timeout_m * 60000) + (timeout_s * 1000) + (timeout_ms * 10);
-  var hello = {{ $maxtime_m }};
-  $('#messages').append("<p>"+"hello"+"</p>");
-});
