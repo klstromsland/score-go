80d84af8cb2430e5a29967ac556bc21e421ad199
Kari Stromsland <klstromsland@gmail.com>
2018-04-25 07:11:04 -0700

    timer test

--- static/jscript/getTime.js
+++ static/jscript/getTime.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
   $("#sw_status").empty();
-  $("#sw_status").append("Ready"); 
+  $("#sw_status").append("Ready");
   var $start = "";
   $("#sw_start").click(function(){
     $("#sw_status").empty();
-    $("#sw_status").append("Started"); 
+    $("#sw_status").append("Started");
     $start = setInterval("delayedPost()", 10);
-  }); 
+  });
   $("#sw_pause").click(function(){
     $("#sw_status").empty();
-    $("#sw_status").append("Stopped"); 
+    $("#sw_status").append("Stopped");
     clearInterval($start);
   });
 //  $("#sw_reset").click(function(){
 //    $("#sw_status").empty();
-//    $("#sw_status").append("Reset"); 
+//    $("#sw_status").append("Reset");
 //    clearInterval($start);
-//  });  
+//  });
 });
 function delayedPost() {
-$.post("http://localhost:8080/gettime", "", function(data, status) {
+$.post("https:/intense-gorge-68089.herokuapp.com/gettime", "", function(data, status) {
    $("#timeread").empty();
    $("#timeread").append(data);
  });
