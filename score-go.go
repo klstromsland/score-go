9731b36662bc42a1232395afd546a0303f39737e
Kari Stromsland <klstromsland@gmail.com>
2018-04-26 10:41:29 -0700

    interval 20 delay 40

--- score-go.go
+++ score-go.go
@@ -66,7 +66,7 @@ var repeat int
 const second = time.Second
 const minute = time.Minute
 const millisecond = time.Millisecond
-const jqDelay = 25*millisecond
+const jqDelay = 40*millisecond
 
 
 //Global structs//////////////////////////////////////////////////////////////////////////////////////
